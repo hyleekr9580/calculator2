f9708f0f834ed6a005c607fbce7f365f9a1e217a
hoyoung lee <hoyoung lee>
2016-04-05 15:08:01 +0900

    부가세계산기2

--- app/src/main/java/contentsstudio/kr/calculator2/MainActivity.java
+++ app/src/main/java/contentsstudio/kr/calculator2/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText mEditMoney;
     private EditText mEditVat;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
