96bf685ee23e866a677dee2da5f049b6c1cae1ae
hoyoung lee <hoyoung lee>
2016-04-05 16:48:59 +0900

    빈값처리

--- app/src/main/java/contentsstudio/kr/calculator2/MainActivity.java
+++ app/src/main/java/contentsstudio/kr/calculator2/MainActivity.java
@@ -5,33 +5,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText mEditTotal;
     private EditText mEditMoney;
     private EditText mEditVat;
 
+    private Button mButtonClean;
+
+    private String reault = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mEditTotal = (EditText) findViewById(R.id.total_edt);
         mEditMoney = (EditText) findViewById(R.id.money_edt);
         mEditVat = (EditText) findViewById(R.id.vat_edt);
 
+        mButtonClean = (Button) findViewById(R.id.clean_btn);
+        mButtonClean.setOnClickListener(this);
 
         mEditTotal.addTextChangedListener(watcher1);
         mEditMoney.addTextChangedListener(watcher2);
+        mEditVat.addTextChangedListener(watcher3);
 
         mEditTotal.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (hasFocus) {
                     mEditMoney.removeTextChangedListener(watcher2);
+                    mEditVat.removeTextChangedListener(watcher3);
                 } else {
                     mEditMoney.addTextChangedListener(watcher2);
+                    mEditVat.addTextChangedListener(watcher3);
                 }
             }
         });
@@ -41,12 +52,33 @@ public class MainActivity extends AppCompatActivity {
             public void onFocusChange(View v, boolean hasFocus) {
                 if (hasFocus) {
                     mEditTotal.removeTextChangedListener(watcher1);
+                    mEditVat.removeTextChangedListener(watcher3);
+
                 } else {
                     mEditTotal.addTextChangedListener(watcher1);
+                    mEditVat.addTextChangedListener(watcher3);
+
+                }
+            }
+        });
+
+        mEditVat.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    mEditTotal.removeTextChangedListener(watcher1);
+                    mEditMoney.removeTextChangedListener(watcher2);
+
+                } else {
+                    mEditTotal.addTextChangedListener(watcher1);
+                    mEditMoney.addTextChangedListener(watcher2);
+
                 }
             }
         });
     }
+
 
     //  합계를 입력 하였을때 공급가, 부가세 자동변경
     TextWatcher watcher1 = new TextWatcher() {
@@ -60,20 +92,25 @@ public class MainActivity extends AppCompatActivity {
         public void onTextChanged(CharSequence s, int start, int before, int count) {
             // 입력되는 텍스트에 변화가 있을 때
 
-            float num1, res, vat;
+            float num, res, vat;
 
-            num1 = Float.parseFloat(mEditTotal.getText().toString());
+            if (s.toString().equals(reault)) {
+                mEditMoney.setText("");
+                mEditVat.setText("");
+            } else {
 
-            //  공급가 계산
-            res = num1 / 1.1F;
-            //  부가세 계산
-            vat = num1 - res;
+                num = Float.parseFloat(mEditTotal.getText().toString());
 
-            //  공급가 보여주기
-            mEditMoney.setText(String.format("%.1f", res));
-            //  부가세 보여주기
-            mEditVat.setText(String.format("%.1f", vat));
+                //  공급가 계산
+                res = num / 1.1F;
+                //  부가세 계산
+                vat = num - res;
 
+                //  공급가 보여주기
+                mEditMoney.setText(String.format("%,6.1f", res));
+                //  부가세 보여주기
+                mEditVat.setText(String.format("%,6.1f", vat));
+            }
         }
 
         @Override
@@ -93,20 +130,60 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-            float num2, res, vat;
+            float num, res, vat;
+            if (s.toString().equals(reault)) {
+                mEditTotal.setText("");
+                mEditVat.setText("");
+            } else {
+                num = Float.parseFloat(mEditMoney.getText().toString());
 
-            num2 = Float.parseFloat(mEditMoney.getText().toString());
-
-            //  부가세 계산
-            vat = num2 * 0.1F;
-            //  합계 계산
-            res = num2 + vat;
+                //  부가세 계산
+                vat = num * 0.1F;
+                //  합계 계산
+                res = num + vat;
 
 
-            //  합계 보여주기
-            mEditTotal.setText(String.format("%.1f", res));
-            //  부가세 보여주기
-            mEditVat.setText(String.format("%.1f", vat));
+                //  합계 보여주기
+                mEditTotal.setText(String.format("%,6.1f", res));
+                //  부가세 보여주기
+                mEditVat.setText(String.format("%,6.1f", vat));
+            }
+        }
+
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+    //  부가세를 입력 하였을때 합계, 공급가 자동변경
+
+    TextWatcher watcher3 = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            float num, res, total;
+            if (s.toString().equals(reault)) {
+                mEditMoney.setText("");
+                mEditTotal.setText("");
+            } else {
+                num = Float.parseFloat(mEditVat.getText().toString());
+
+                //  공급가 계산
+                res = num * 10;
+                //  합계 계산
+                total = num + res;
+
+
+                //  공급가 보여주기
+                mEditMoney.setText(String.format("%,6.1f", res));
+                //  합계 보여주기
+                mEditTotal.setText(String.format("%,6.1f", total));
+            }
         }
 
         @Override
@@ -114,5 +191,15 @@ public class MainActivity extends AppCompatActivity {
 
         }
     };
+
+    //  클릭 이벤트
+    @Override
+    public void onClick(View v) {
+
+        Toast.makeText(MainActivity.this, "모든 항목이 삭제 되었습니다.", Toast.LENGTH_SHORT).show();
+        mEditTotal.setText("");
+        mEditMoney.setText("");
+        mEditVat.setText("");
+    }
 }
 
